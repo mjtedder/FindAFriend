e30a7b24ba419406f9a6f4bb5c9b3910f99ed85e
mjtedder <mjtedder@gmail.com>
2018-06-16 20:44:21 -0400

    array is now capturing data successfully

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,20 @@
 let friendsArray = require("../data/friends");
 
+//Logic for comparing differences in scores
+//for (let i = 1; i < 2; i++) {
+   // math.abs()
+//}
+
 //ROUTING
-//API GET request
 module.exports = function (app) {
+    //API GET request
     app.get("/api/friends", function (req, res) {
         res.json(friendsArray);
     });
 
     //API POST request for adding new friend profile to the array
     app.post("/api/friends", function (req, res) {
+        friendsArray.push(req.body);
         res.json(friendsArray);
     });
 }
--- server.js
+++ server.js
@@ -9,8 +9,8 @@ let PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-require('./app/routing/apiRoutes')(app)
-require('./app/routing/htmlRoutes')(app)
+require(path.join(__dirname, './app/routing/apiRoutes'))(app)
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app)
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
