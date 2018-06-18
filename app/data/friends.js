40624d099265d21da13c77ae346e25d9bb4cb075
mjtedder <mjtedder@gmail.com>
2018-06-18 10:40:51 -0400

    logic complete, need to get modals functioning

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,18 @@
-let friendsArray = [];
+let friendsArray = [{
+    name: 'Matt Tedder',
+    photo: 'https://avatars3.githubusercontent.com/u/36042608?s=460&v=4',
+    scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+    ]
+}];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,53 @@
 let friendsArray = require("../data/friends");
 
-//Logic for comparing differences in scores
-//for (let i = 1; i < 2; i++) {
-   // math.abs()
-//}
+
+module.exports = function(app) {
+    // A GET route that displays JSON of all possible friends
+    app.get('/api/friends', function(req,res){
+        res.json(friendsArray);
+    });
+    // A POST route that displays new friends scores to compare with possible friends
+    app.post('/api/friends', function(req,res){
+        let newFriendScores = req.body.scores;
+        let scoresArray = [];
+        let friendCount = 0;
+        let bestMatch = 0;
+
+    //Looping through friends list to compare with friends in friendsArray
+    for (let i = 0; i < friendsArray.length; i++) {
+        let scoresDiff = 0;
+        //Run through scores to compare friends
+        for (let j = 0; j < newFriendScores.length; j++) {
+            scoresDiff += (Math.abs(parseInt(friendsArray[i].scoresArray[j]) - parseInt(newFriendScores[j])));
+        }
+        //Push results into scoresArray
+        scoresArray.push(scoresDiff)
+    }
+        //After friends are compared, find the best match
+        for (let i = 0; i < scoresArray.length; i++) {
+            if(scoresArray[i] <= scoresArray[bestMatch]){
+                bestMatch = i;
+            }
+        }
+
+        //returns bestMatch
+        let bff = friendsArray[bestMatch];
+        res.json(bff);
+
+        //pushing new submission into friendsArray
+        friendsArray.push(req.body);
+    });
+};
+
+
+//function findMatch
+ function findFriend() {
+     let result;
+     let score = 50;
+     for (friend of friends) {
+        console.log(friend);
+     }
+ }
 
 //ROUTING
 module.exports = function (app) {
