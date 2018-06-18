9a934daaebc80872f7d2dfbbe08e9ce1a686d984
mjtedder <mjtedder@gmail.com>
2018-06-18 11:26:27 -0400

    Can't get modals to display, address later, must move on to next project.

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,16 +2,16 @@ let friendsArray = [{
     name: 'Matt Tedder',
     photo: 'https://avatars3.githubusercontent.com/u/36042608?s=460&v=4',
     scores: [
-        "4",
-        "5",
-        "5",
-        "2",
-        "4",
-        "5",
-        "4",
-        "4",
-        "3",
-        "4"
+        4,
+        5,
+        5,
+        2,
+        4,
+        5,
+        4,
+        4,
+        3,
+        4
     ]
 }];
 
